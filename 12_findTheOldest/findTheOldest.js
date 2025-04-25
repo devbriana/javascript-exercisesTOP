be5a6c4ce0e14d70b85f98216147fdd51de1d992
devbriana <dev.brianam@gmail.com>
2025-04-25 19:53:11 -0400

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let yearsAlive = array.map(person => ({
+        ...person,
+        age: (person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth
+    }));
+    return yearsAlive.sort((a, b) => {
+        if (a.age > b.age) {
+            return -1
+        } else if (a.age < b.age) {
+            return 1
+        }
+    }) [0];
 
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
