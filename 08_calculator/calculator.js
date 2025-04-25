4d5028a2f4869be4721b81ea52e732f14d869a15
devbriana <dev.brianam@gmail.com>
2025-04-24 21:06:01 -0400

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((accum, item) => accum + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((accum, item) => accum * item);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let emptyArr = [];
+  if (num === 0) {
+    return 1;
+  }
+  for (let i = num; i >= 1; i--) {
+    emptyArr.push(i);
+  }
+  return emptyArr.reduce((accum, item) => accum * item);
 };
 
 // Do not edit below this line
