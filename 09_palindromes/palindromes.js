9b680770bfbff353c41dce0275edafc947c76292
devbriana <dev.brianam@gmail.com>
2025-04-24 22:19:13 -0400

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function(string) {
+    string = string.toLowerCase().replace(/[^a-z0-9]/gi, '');
+    let stringArr = string.split('');
+    console.log(stringArr);
+    let regString = stringArr.join('');
+    console.log(regString);
+    let reversedArr = stringArr.reverse(); 
+    let reversedString = reversedArr.join('');
+    console.log(reversedString);
+    if (regString === reversedString) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
