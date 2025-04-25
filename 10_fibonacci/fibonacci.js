a0025b30795bd619f4165fd00277755fb992fff4
devbriana <dev.brianam@gmail.com>
2025-04-25 18:10:35 -0400

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let arr = [1, 1];
+    if (n === String) {
+        Number(n);
+    }
+    if (n === 0 || n === '0') {
+        return 0;
+    } 
+    if (n < 0) {
+        return "OOPS";
+    }
+    for (let i = 2; i < n; i++) {
+        let firstNum = arr[i - 1]; // (arr[1])
+        let secondNum = arr[i - 2]; // (arr[0])
+        let nextNum = firstNum + secondNum;
+        arr.push(nextNum);   
+    }
+    console.log(arr);
+    console.log(arr[n-1]);
+    return(arr[n-1]);
 };
 
 // Do not edit below this line
